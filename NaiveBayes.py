c5f0a4f5e84eb1e8594e423a936b269b75fae1c4
Oliver Holland <odholl@essex.ac.uk>
2016-11-28 18:36:24 +0000

    Return instead of print in def extract_vocab. Created a new def read_samples() that reads in sampleTrain.txt and strips/splits it by tab

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,7 +1,15 @@
 def extract_vocab():
     with open('sampleTrain.vocab.txt') as f:
         words = f.read().splitlines()
-        print ({word: index for index, word in enumerate(words)})
+        return {word: index for index, word in enumerate(words)}
+
+def read_samples():
+    with open("sampleTrain.txt") as f:
+        string_samples = f.read().splitlines()
+        for sample in string_samples:
+            sample = sample.strip()
+            sample_elements = sample.split('\t')
+            print (sample_elements)
 
 
-extract_vocab()
+read_samples()
