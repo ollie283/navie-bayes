332cb0b693e7a7aff2cc544017e461f08fca9303
Oliver Holland <odholl@essex.ac.uk>
2016-11-28 18:59:14 +0000

    Return samples rather than print in def read_samples. In addition added path as a fuction call. Created a new def train_model that gets the real class (1 or 0) and keeps a count

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -3,17 +3,25 @@ def extract_vocab():
         words = f.read().splitlines()
         return {word: index for index, word in enumerate(words)}
 
-def read_samples():
-    with open("sampleTrain.txt") as f:
+def read_samples(path):
+    with open(path) as f:
         samples = []
         string_samples = f.read().splitlines()
         for sample in string_samples:
             sample = sample.strip()
             sample_elements = sample.split('\t')
-            document_name = sample_elements[0]
+            document_id = sample_elements[0]
             real_class = int(sample_elements[1])
             words = sample_elements[2].split(' ')
-            samples.append((document_name, real_class, words))
-        print (samples)
+            samples.append((document_id, real_class, words))
+        return samples
 
-read_samples()
+def train_model():
+    prior_probabilities = {}
+    samples = read_samples('sampleTrain.txt')
+    for sample in samples:
+        prior_probabilities[sample[1]] = prior_probabilities.get(sample[1], 0.0) + 1.0
+        
+    print (prior_probabilities)
+
+train_model()
